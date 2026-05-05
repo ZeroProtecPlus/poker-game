ce5aca125027533bb87848a0ea1af9d5a97bc37e
ZeroProtecPlus <jsebastiangiraldo@ucompensar.edu.co>
2026-05-04 19:09:17 -0500

    fix(controller): update chips display immediately after resume modal

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -177,6 +177,7 @@ public class GameController {
                 boolean resume = newGame.askResumeGame(state.getHumanChips());
                 if (resume) {
                     newPlayer.setChips(state.getHumanChips());
+                    newGame.showUserChips(userNamePlayer, newPlayer.getNumbChips());
                 } else {
                     gameRepository.deleteByMachineId(machineId);
                 }
